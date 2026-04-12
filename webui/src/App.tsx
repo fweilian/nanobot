1dc9353798329c1a548dc1af4442372b55697872
fweilian <fweilian@163.com>
2026-04-12 14:36:37 +0800

    feat(webui): add dark mode theme support

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import { MainLayout } from './components/Layout/MainLayout';
+import { useConfigStore } from './stores/configStore';
 
 function App() {
+  const { theme } = useConfigStore();
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
   return <MainLayout />;
 }
 
